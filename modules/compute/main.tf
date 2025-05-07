ad371b63479bbfe8ddab2a390471bb302df6b4d6
Sooraj7340 <surajgoudar2001@gmail.com>
2025-05-07 11:02:30 +0530

    server key updated

--- modules/compute/main.tf
+++ modules/compute/main.tf
@@ -8,7 +8,7 @@ resource "aws_instance" "jump_server" {
   instance_type          = "t2.micro"
   subnet_id              = var.subnet_id
   vpc_security_group_ids = [var.sg_id]
-  key_name               = "my-key"
+  key_name               = "key"
 
   tags = {
     Name = "JumpServer"
